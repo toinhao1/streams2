ee61574d13ec853ee785ac9d7c175016dbde84f3
toinhao1 <toinhao89@gmail.com>
2019-07-02 16:15:23 +0200

    GAuth almost set up

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import GoogleAuth from './GoogleAuth';
 
 const Header = () => {
   return (
@@ -9,6 +10,7 @@ const Header = () => {
       </Link>
 
       <div className="right menu">
+        <GoogleAuth />
         <Link to="/" className="item">
           All Streams
         </Link>
